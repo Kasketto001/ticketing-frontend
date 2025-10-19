aad5144e541194eb282e32a9615aee176cf65fe8
Kasketto <kaskettomaledetto@gmail.com>
2025-10-19 19:05:19 +0200

    add .env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosHeaders } from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.REACT_APP_API_URL,
 });
 
 // Read accessToken from Zustand persisted storage (key: "auth-storage").
